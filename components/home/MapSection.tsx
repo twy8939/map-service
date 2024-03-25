1b8e026e91e1e260cf087509aabb5236f7729ad1
twy8939 <twy8939@naver.com>
2024-03-26 01:03:40 +0900

    feat : url 위치 유지하기

--- components/home/MapSection.tsx
+++ components/home/MapSection.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Map from './Map';
 import { NaverMap } from '@/types/map';
-import useMap from '../../hooks/useMap';
+import useMap, { INITAL_ZOOM, INITIAL_CENTER } from '../../hooks/useMap';
 import Markers from './Markers';
 import useCurrentStore from '@/hooks/useCurrentStore';
+import { useRouter } from 'next/router';
+import { Coordinates } from '@/types/store';
 
 const MapSection = () => {
+  const router = useRouter();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const query = useMemo(() => new URLSearchParams(router.asPath.slice(1)), []);
+  const initialZoom = useMemo(
+    () => (query.get('zoom') ? Number(query.get('zoom')) : INITAL_ZOOM),
+    [query]
+  );
+  const initialCenter = useMemo<Coordinates>(
+    () =>
+      query.get('lat') && query.get('lng')
+        ? [Number(query.get('lat')), Number(query.get('lng'))]
+        : INITIAL_CENTER,
+    [query]
+  );
+
   const { initializeMap } = useMap();
   const { clearCurrentStore } = useCurrentStore();
 
@@ -15,7 +32,11 @@ const MapSection = () => {
   };
   return (
     <>
-      <Map onLoad={onLoadMap} />
+      <Map
+        onLoad={onLoadMap}
+        initialZoom={initialZoom}
+        initialCenter={initialCenter}
+      />
       <Markers />
     </>
   );
