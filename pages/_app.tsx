afa6bad4b9fb944488da2d975dbac79070257371
twy8939 <twy8939@naver.com>
2024-04-09 00:04:03 +0900

    feat : Google Analytics script 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,28 @@ import type { AppProps } from 'next/app';
 
 import SEO from '@/seo.config';
 import { DefaultSeo } from 'next-seo';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
