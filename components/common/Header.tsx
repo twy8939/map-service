a15892f6ba9b2953735a1b40301c56872a91c3b9
twy8939 <twy8939@naver.com>
2024-04-14 18:46:15 +0900

    feat : next/link > legacyBehavior 적용

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { ComponentPropsWithRef, ForwardedRef } from 'react';
 import styles from '@/styles/header.module.scss';
 
 interface HeaderProps {
@@ -8,17 +8,27 @@ interface HeaderProps {
   rightElements?: React.ReactNode;
 }
 
+const CustomAnchor = React.forwardRef(function CustomAnchor(
+  props: ComponentPropsWithRef<'a'>,
+  ref: ForwardedRef<HTMLAnchorElement>
+) {
+  return (
+    <a ref={ref} {...props}>
+      <Image src="/logo.webp" width={110} height={30} alt="logo" priority />
+    </a>
+  );
+});
+
 export default function Header({ onClickLogo, rightElements }: HeaderProps) {
   return (
     <header className={styles.header}>
       <div className={styles.flexItem}>
-        <Link
-          href="/"
-          className={styles.box}
-          onClick={onClickLogo}
-          aria-label="홈으로 이동"
-        >
-          <Image src="/logo.webp" width={110} height={30} alt="logo" priority />
+        <Link href="/" legacyBehavior passHref>
+          <CustomAnchor
+            className={styles.box}
+            onClick={onClickLogo}
+            aria-label="홈으로 이동"
+          />
         </Link>
       </div>
       {rightElements && <div className={styles.flexItem}>{rightElements}</div>}
