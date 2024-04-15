f4b123947a503eea7116715aa02d6bcf35b8e9cf
twy8939 <twy8939@naver.com>
2024-04-15 21:46:32 +0900

    feat : HeadingsMap 구조 개선

--- components/home/DetailHeader.tsx
+++ components/home/DetailHeader.tsx
@@ -25,7 +25,7 @@ const DetailHeader = ({ expanded, handleArrowClick, currentStore }: Props) => {
       {!currentStore && <p className={styles.title}>매장을 선택해주세요</p>}
       {currentStore && (
         <div className={styles.flexRow}>
-          <p className={styles.title}>{currentStore.name}</p>
+          <h1 className={styles.title}>{currentStore.name}</h1>
           <button
             className={styles.shareButton}
             onClick={() => {
